9403b310ed3b48da1e7d8866e4fa9901ee90dbae
Luong Viet Dung <l-dung@ruby-dev.jp>
2018-06-11 11:28:17 +0700

    Implement Share twitter and facebook

--- app/javascript/components/products/Actions.vue
+++ app/javascript/components/products/Actions.vue
@@ -8,10 +8,22 @@
         </button>
 
         <div class="psr dib">
-          <button class="btn share ml-small">
+          <button class="btn share ml-small" @click="toggleShare">
             <i class="ion-android-share-alt" />
             <span class="ml-small">Share</span>
           </button>
+          <div class="share-list animated bounceIn" v-if="showShareList"
+            v-click-outside:delay="toggleShare">
+            <a class="twitter"
+              href="http://twitter.com/home?status=Check the Vue Mini Shop Out! It's either example App, built with Vue js https://bosnaufal.github.io/vue-mini-shop #vuejs #javascript #rails">
+              <i class="ion-social-twitter" />
+              <span>Twitter</span>
+            </a>
+            <a href="http://www.facebook.com/share.php?u=https://bosnaufal.github.io/vue-mini-shop">
+              <i class="ion-social-facebook" />
+              <span>Facebook</span>
+            </a>
+          </div>
         </div>
       </div>
       <div class="col-sm-5 text-right">
@@ -22,5 +34,21 @@
 </template>
 
 <script>
-  export default {};
+  import ClickOutside from 'vue-click-outside'
+
+  export default {
+    data: function() {
+      return {
+        showShareList: false
+      }
+    },
+    methods: {
+      toggleShare: function() {
+        this.showShareList = !this.showShareList;
+      }
+    },
+    directives: {
+      ClickOutside
+    }
+  };
 </script>
